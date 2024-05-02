95a42463698cca758f7f4a28229c8983d663cf58
Joshua Federman <97866996+JoshTheConfused@users.noreply.github.com>
2024-05-02 22:08:24 +0000

    Made a basic framework for the project

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-console.log('hi');
+let highText = document.getElementById("high");
+let lowText = document.getElementById("low");
+let high;
+// high = {api call} => high temp as int
+let low;
+// low = {api call} => low temp as int
+let rain;
+// rain = {api call} => 0:none, 1:moderate, 2:heavy
 
-let heading = document.querySelector("h1");
+// highText.innerHTML = `high: ${high}`
+// lowText.innerHTML = `low: ${low}`
 
-heading.addEventListener("click", function(){
-    heading.style.color = 'green';
-    heading.innerText = 'Ready to Code';
-});
+let headwearDiv = document.getElementById("headwear");
+let jacketDiv = document.getElementById("jacket");
+let shirtDiv = document.getElementById("shirt");
+let pantsDiv = document.getElementById("pants");
+let shoesDiv = document.getElementById("shoes");
+
+// based on the weather data, determine the appropriate outfit, and load the corresponding description and image for each piece of clothing
+
+// sign up for api here: https://openweathermap.org/api
