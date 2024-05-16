0d2996b38d2a2624ad561a9887882ebe29fbb34d
Joshua Federman <97866996+JoshTheConfused@users.noreply.github.com>
2024-05-16 22:01:56 +0000

    completed the weather retrieval process

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let highText = document.getElementById("high");
-let lowText = document.getElementById("low");
-let high;
-// high = {api call} => high temp as int
-let low;
-// low = {api call} => low temp as int
-let rain;
-// rain = {api call} => 0:none, 1:moderate, 2:heavy
-
-// highText.innerHTML = `high: ${high}`
-// lowText.innerHTML = `low: ${low}`
+const key = "3e9bc118a4df0e6fbe5825567671d11d";
+const lat = 40.71423095312353; 
+const lon = -73.97922364701996;
+let temp;
+let weather;
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`).then(response => response.json()).then(data => {
+  console.log(data);
+  temp = data.main.temp;
+  weather = data.weather[0];
+  
+  document.getElementById("temp").innerHTML = `Temp: ${Math.round(temp)} degrees Fahrenheit`;
+  document.getElementById("precip").innerHTML = `Precipitation: ${precipIntensity(weather)}`
+});
 
 let headwearDiv = document.getElementById("headwear");
 let jacketDiv = document.getElementById("jacket");
@@ -18,4 +20,55 @@ let shoesDiv = document.getElementById("shoes");
 
 // based on the weather data, determine the appropriate outfit, and load the corresponding description and image for each piece of clothing
 
-// sign up for api here: https://openweathermap.org/api
+function precipIntensity(weather) {
+  let intensity; // 0 = no precipitation --> 5 = heavy precipitation
+  let desc = weather.description;
+  if (desc.includes("very heavy")) {
+    intensity = 5;
+  }
+  else if (desc.includes("heavy")) {
+    if (desc.includes("rain") || desc.includes("shower") || desc.includes("snow")) {
+      intensity = 4;
+    }
+    else if (desc.includes("drizzle")) {
+      intensity = 3;
+    }
+  }
+  else if (desc.includes("light")) {
+    if (desc.includes("rain") || desc.includes("snow")) {
+      intensity = 2;
+    }
+    else if (desc.includes("drizzle")) {
+      intensity = 1;
+    }
+  }
+  else if (weather.id < 700) {
+    intensity = 3;
+  }
+  else {
+    intensity = 0;
+  }
+
+  let myDesc;
+  switch (intensity) {
+    case 0:
+      myDesc = "No precipitation";
+      break;
+    case 1:
+      myDesc = "Very light precipitation";
+      break;
+    case 2:
+      myDesc = "Light precipitation";
+      break;
+    case 3:
+      myDesc = "Moderate precipitation";
+      break;
+    case 4:
+      myDesc = "Heavy precipitation";
+      break;
+    case 5:
+      myDesc = "Very heavy precipitation";
+      break;
+  }
+  return myDesc;
+}
